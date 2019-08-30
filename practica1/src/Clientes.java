aef49ad98b82c63e12b90f0d6a5698fc190030cd
Caams CM <ccmiranda@ciencias.unam.mx>
2019-08-29 23:46:42 -0500

    Hice un esqueleto para los clientes y los servicios

--- practica1/src/Clientes.java
+++ practica1/src/Clientes.java
@@ -1,3 +1,14 @@
 public class Clientes{
-    
+
+    private String nombre;
+    private int dinero;
+    private String usuario;
+
+    public void suscribir(String servicio){
+
+    }
+
+    public void cancelarServicio(String servicio){
+        
+    }
 }
--- practica1/src/Servicios.java
+++ practica1/src/Servicios.java
@@ -1,3 +1,30 @@
-public class Servicios{
-    
+import java.util.ArrayList;
+
+abstract public class Servicios{
+
+    private String nombre;
+    private int costo;
+    private ArrayList<String> suscriptores;
+
+    public void cobrar(String usuario){
+
+    }
+
+    public String cancelarServicio(){
+
+    }
+
+    public String notificarCobro(){
+
+    }
+
+    public String recomendar(){
+
+    }
+
+    public String bienvenida(){
+
+    }
+
+
 }
