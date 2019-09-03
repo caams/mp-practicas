8237b182b50bdecae6cd93fd2e4ea89b70ee5abc
Caams CM <ccmiranda@ciencias.unam.mx>
2019-09-03 14:50:11 -0500

    renombré a Servicio y creé el main

--- practica1/src/Servicio.java
+++ practica1/src/Servicio.java
@@ -6,6 +6,18 @@ abstract public class Servicios{
     private int costo;
     private ArrayList<String> suscriptores;
 
+    /**
+     * 
+     * @param nombre
+     * @param costo
+     * @param l
+     */
+    public Servicio(String nombre, int costo, ArrayList<String> l){
+        this.nombre = nombre;
+        this.costo = costo;
+        this.l = suscriptores;
+    }
+
     public void cobrar(String usuario){
 
     }
