b3b1510542fad35dca6a2d2f915e1f49bea63a39
Caams CM <ccmiranda@ciencias.unam.mx>
2019-09-04 10:56:09 -0500

    Cree a los clientes que debemos usar

--- practica1/src/Practica1.java
+++ practica1/src/Practica1.java
@@ -6,7 +6,11 @@ public class Practica1{
     public static void main(String[] args) {
         
         System.out.println("Empezando simulación...");
-
-        Cliente Ana = new Cliente(Alicia, 500);
+        
+        Cliente Ana = new Cliente("Alicia", 500);
+        Cliente Bob = new Cliente("Bob", 40);
+        Cliente Cesar = new Cliente("César", 40);
+        Cliente Diego = new Cliente("Diego", 80);
+        Cliente Erika = new Cliente("Erika", 300);
     }
 }
