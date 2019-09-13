ff0c95e00a1ea9e7c2f9ba45555fd7fd531ecef7
Caams CM <ccmiranda@ciencias.unam.mx>
2019-09-13 11:09:29 -0500

    Mis cambios de hamburguesa

---
+++ practica2/src/PrepararHamburguesa.java
@@ -0,0 +1,42 @@
+public abstract class PrepararHamburguesa {
+
+    /*
+	* Metodo abstracto para que cada clase lo modifique a su antojo.
+	*/
+    abstract void prepararCarne();
+    
+    abstract void ponerCarne();
+
+	abstract void ponerQueso();
+
+	/*
+	* Metodo template (plantilla)
+	*/
+	public final void rutinaDiaria(){
+        ponerPan();
+        ponerMayonesa();
+        prepararCarne();
+        ponerCarne();
+        ponerQueso();
+        ponerVegetales();
+        ponerCatsup();
+        ponerPan();
+	}
+
+	void ponerPan(){
+		System.out.println("\nPoner pan [ -c°▥°]-c. Estoy poniendo el pan.");
+	}
+
+	void ponerMayonesa(){
+		System.out.println("Poner mayonesa [ -c°▥°]-c. Estoy untando la mayonesa en el pan.");
+	}
+
+	void ponerVegetales(){
+		System.out.println("Poner vegetales [ -c°▥°]-c. Estoy poniendo los vegetales.");
+	}
+
+	void ponerCatsup(){
+		System.out.println("Poner catsup [ -c°▥°]-c. Estoy poniendo catsup.");
+	}
+	
+}
